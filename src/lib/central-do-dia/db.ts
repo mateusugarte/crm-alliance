26c585a53709ff2beb85a9a1e7c8f4d41f5a0bc5
Marco Amorim <marcoantonioamorim99@gmail.com>
2026-08-05 15:15:12 -0300

    Valida TLS do banco em producao

--- src/lib/central-do-dia/db.ts
+++ src/lib/central-do-dia/db.ts
@@ -8,10 +8,20 @@ function getPool() {
     throw new Error('PG_MEMORY_URL nao configurada')
   }
 
+  const sslCaBase64 = process.env.PG_SSL_CA_BASE64
+  if (process.env.NODE_ENV === 'production' && !sslCaBase64) {
+    throw new Error('PG_SSL_CA_BASE64 nao configurada')
+  }
+
   if (!globalForCentral.centralPool) {
     globalForCentral.centralPool = new Pool({
       connectionString: process.env.PG_MEMORY_URL,
-      ssl: { rejectUnauthorized: false },
+      ssl: sslCaBase64
+        ? {
+            ca: Buffer.from(sslCaBase64, 'base64').toString('utf8'),
+            rejectUnauthorized: true,
+          }
+        : { rejectUnauthorized: false },
       max: 4,
       connectionTimeoutMillis: 5_000,
       idleTimeoutMillis: 20_000,
